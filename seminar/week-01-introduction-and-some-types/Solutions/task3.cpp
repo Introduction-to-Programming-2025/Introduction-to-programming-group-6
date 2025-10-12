5f50a51bebdfbac0d1e149712a400e8d55bb3491
Ivaylo Petkov <ivailo2000petkov@gmail.com>
2025-10-12 21:32:47 +0300

    [Seminar] Fix solution

--- seminar/week-01-introduction-and-some-types/Solutions/task3.cpp
+++ seminar/week-01-introduction-and-some-types/Solutions/task3.cpp
@@ -6,7 +6,7 @@ int main() {
     int a, b, c, x;
     cin >> a >> b >> c >> x;
     
-    cout << "a*x^2 + b*x + c = " << ( a*x^2 + b*x + c ) << endl;
+    cout << "a*x^2 + b*x + c = " << ( a*x*x + b*x + c ) << endl;
 
     return 0;
 }

4204922384e9a872492f6b2a6d409bb472bf6097
Boris Troharov <snowblybg@gmail.com>
2025-10-16 13:42:59 +0300

    week-2-pract

---
+++ practicum/week-01-primitive-data-types/solutions.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <cmath>
+int main()
+{
+    // Задача 1
+    // char c;
+    // std::cin >> c;
+    // std::cout << (c >= 'a' && c <= 'z') << std::endl;
+    // Задача 2
+    // char a, b;
+    // std::cin >> a >> b;
+    // unsigned char sum = (a - '0') + (b - '0');
+    // char edinici = sum % 10 + '0';
+    // char desetici = sum / 10 + '0';
+    // std::cout << "Sum -> " << "Desetici: " << desetici << " Edinici: " << edinici << std::endl;
+    // Задача 3
+    // int a, b;
+    // std::cin >> a >> b;
+    // bool result = (b != 0 && a % b == 0) || (a != 0 && b % a == 0);
+    // std::cout << result << std::endl;
+    // Задача 4
+    // long minutes;
+    // std::cout << "Minutes: ";
+    // std::cin >> minutes;
+    // int years = minutes / (60 * 24 * 365);
+    // minutes %= (60 * 24 * 365);
+    // int days = minutes / (60 * 24);
+    // minutes %= (60 * 24);
+    // int hours = minutes / 60;
+    // std::cout << "Years: " << years << " Days: " << days << " Hours: " << hours << std::endl;
+    // Задача 5
+    // double price;
+    // int discount;
+    // std::cout << "Price: ";
+    // std::cin >> price;
+    // std::cout << "Discount: ";
+    // std::cin >> discount;
+    // double finalPrice = price - (price * discount / 100.0);
+    // double rounded = (int)(finalPrice * 100 + 0.5) / 100.0;
+    // std::cout << "Final Price: " << rounded << std::endl;
+    // Задача 6
+    // int a, b, c;
+    // std::cin >> a >> b >> c;
+    // double p = (a + b + c) / 2.0;
+    // double s = sqrt(p * (p - a) * (p - b) * (p - c));
+    // std::cout << s << std::endl;
+    // Задача 7
+    // int n;
+    // std::cin >> n;
+    // int ones = n % 10;
+    // int tens = (n / 10) % 10;
+    // int hundreds = (n / 100) % 10;
+    // int thousands = n / 1000;
+    // int reversed = ones * 1000 + tens * 100 + hundreds * 10 + thousands;
+    // std::cout << (reversed == n) << std::endl;
+    // Задача 8а
+    // double x, y;
+    // std::cin >> x >> y;
+    // bool inside = (x * x + y * y <= 25) && (y >= x) && x >= 0;
+    // std::cout << inside << std::endl;
+    // Задача 8б
+    // double x, y;
+    // std::cin >> x >> y;
+    // bool inside = (x * x + y * y <= 25) && (x - 5) * (x - 5) + (y - 5) * (y - 5) <= 25;
+
+    // std::cout << inside << std::endl;
+}
